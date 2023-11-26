e3cb5b3df063685e520790c15ef773fcde13c3a8
cloudxinn <cloudxin@foxmail.com>
2023-11-26 22:35:31 +0800

    add config var

--- config.cpp
+++ config.cpp
@@ -5,10 +5,13 @@
 #include<fstream>
 using std::ifstream;
 using std::ios;
-
 int width = 15, length =15;
 int wall_status[4]={1};
 int obstacle_num = 0;
+double speed = 10;
+int seed=-1;
+int fruit_num = 1;
+int furit_pro[3] = {6,3,1}; //10points express probability
 bool setmap(void)
 {
 	ifstream map("maps/default.map",ios::in);
@@ -28,8 +31,20 @@ bool setmap(void)
 }
 bool setconfig(void)
 {
+	bool exit = false;
+	while(!exit)
+	{	
+	BeginDrawing();
+	ClearBackground(WHITE);
+		exit =  GuiButton((Rectangle){ 520, 680, 1240, 880 }, "返回"); 
+	EndDrawing();
+	}
 	ifstream config("config/default.config",ios::in);
 	if(!config) return false;
+	config >> speed >> seed >> fruit_num;
+	furit_pro[0]=config.get()*10;
+	furit_pro[1]=config.get()*10;
+	furit_pro[2]=config.get()*10;
 	config.close();
 	return true;
 }
