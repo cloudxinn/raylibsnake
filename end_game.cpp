1fdc321c9eb5044ec7f7a72844c698ae921305ca
clyne36 <139964941+clyne36@users.noreply.github.com>
2023-12-03 22:34:55 +0800

    end_game.cpp

--- end_game.cpp
+++ end_game.cpp
@@ -4,9 +4,11 @@
 #include<cstdio>
 void end_game()
 {
+	// 绘制画面
 	BeginDrawing();
-		DrawText("score:",560,280,120,BLACK);
-		DrawText(std::to_string(score).c_str(),600,520,160,BLACK);
+	// 绘制得分文本
+	DrawText("score:",560,280,120,BLACK);
+	DrawText(std::to_string(score).c_str(),600,520,160,BLACK);
 	BeginDrawing();
 	//TODO free space
 	return;
